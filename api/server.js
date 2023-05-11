666cbb4fbbb72c8cd3a447a2019f21017455fd32
Dustin Tatum <dtatum0826@gmail.com>
2023-05-11 18:43:57 -0500

    Get endpoints working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,44 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+const Users = require('./users/model')
+
+const server = express()
+
+
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    Users.findById(req.params.id)
+        .then(user => {
+            if(!user){
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            }else
+            res.json(user)
+        })
+        .catch(err => {
+          { }
+        })
+})
+
+
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found... yet'
+    })
+})
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const server = require('./api/server');
 
 const port = 9000;
 
+
+server.listen(port, () => {
+    console.log("Server started on", port)
+})
 // START YOUR SERVER HERE
