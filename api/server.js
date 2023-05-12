83012b2bbc67f0fd1fb6f863a3e8a6afea3ac0b8
Dustin Tatum <dtatum0826@gmail.com>
2023-05-11 19:17:18 -0500

    Got Delete Endpoint Working

--- api/server.js
+++ api/server.js
@@ -26,7 +26,7 @@ server.get('/api/users/:id', (req, res) => {
                 res.json(user)
         })
         .catch(err => {
-          res.status(500).json({ message: "The user information could not be retrieved"})
+            res.status(500).json({ message: "The user information could not be retrieved" })
         })
 })
 
@@ -34,30 +34,48 @@ server.post('/api/users', (req, res) => {
     const user = req.body
     if (!user.name || !user.bio) {
         res.status(400).json({
-            message: "Please provide name and bio for the user" 
+            message: "Please provide name and bio for the user"
         })
 
     } else {
         Users.insert(user)
-        .then(user =>{
-            res.status(201).json(user)
-        })
-        .catch(err => {
-            res.status(500).json({  message: "There was an error while saving the user to the database"})
-          })
-           
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "There was an error while saving the user to the database" })
+            })
+
     }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    const user = await Users.findById(req.params.id)
+    if (!user) {
+        res.status(404).json({ 
+            message: "The user with the specified ID does not exist" 
+    })
+    } else {
+        const deleted = await Users.remove(req.params.id)
+        res.status(200).json(deleted)
+    }
+    // .then(user => {
+    //     res.json(user)
+    // })
+    //     .catch(err => {
+    //         res.status(500).json({ message: "The user could not be removed" })
+    //     })
 })
 
 
 
 
-    server.use('*', (req, res) => {
-        res.status(404).json({
-            message: 'not found... yet'
-        })
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found... yet'
     })
+})
 
 
 
-    module.exports = server; // EXPORT YOUR SERVER instead of {}
+module.exports = server; // EXPORT YOUR SERVER instead of {}
